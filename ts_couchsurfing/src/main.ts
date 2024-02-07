ee6017a99da63554ef7b8c5abc75a5b8c3be5883
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-02-07 11:19:15 +0100

    first commit

---
+++ ts_couchsurfing/src/main.ts
@@ -0,0 +1,84 @@
+import "./styles/styles.css";
+
+// Object Types Challenge
+// Based on what we discussed we need to make up our Property Objects and array,
+// can you create that array, making sure to assign the correct Types?
+
+import { showReviewTotal, populateUser } from "./utils";
+let isOpen: boolean;
+
+// Reviews
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
+
+// User
+const you: {
+  firstName: string;
+  lastName: string;
+  isReturning: boolean;
+  age: number;
+  stayedAt: string[];
+} = {
+  firstName: "Bobby",
+  lastName: "Brown",
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
+
+//Properties
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [
+  {
+    image: "",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: "marywinkle@gmail.com",
+    isAvailable: true,
+  },
+];
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+populateUser(you.isReturning, you.firstName);
