a66ec4c71856d86107ef5e4b286cc62afbce779f
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-02-07 15:27:50 +0100

    data

--- ts_couchsurfing/src/main.ts
+++ ts_couchsurfing/src/main.ts
@@ -5,6 +5,7 @@ import "./styles/styles.css";
 // can you create that array, making sure to assign the correct Types?
 
 import { showReviewTotal, populateUser } from "./utils";
+const propertyContainer = document.querySelector(".container");
 let isOpen: boolean;
 
 // Reviews
@@ -50,7 +51,9 @@ const you: {
 };
 
 //Properties
+
 const properties: {
+  alt: string;
   image: string;
   title: string;
   price: number;
@@ -64,20 +67,59 @@ const properties: {
   isAvailable: boolean;
 }[] = [
   {
-    image: "",
-    title: "Colombian Shack",
-    price: 45,
+    alt: "https://unsplash.com/photos/a-tall-building-with-a-clock-on-the-top-of-it-7fPm68VRwKQ?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+    image: "poland-property.jpg",
+    title: "Cozy Apartment in Lillafüred",
+    price: 50,
     location: {
-      firstLine: "shack 37",
-      city: "Bogota",
-      code: 45632,
-      country: "Colombia",
+      firstLine: "123 Main Street",
+      city: "Lillafüred",
+      code: 1011,
+      country: "Hungary",
     },
-    contact: "marywinkle@gmail.com",
+    contact: "john.doe@example.com",
+    isAvailable: true,
+  },
+  {
+    alt: "https://unsplash.com/photos/multicolored-concrete-houses-h95mT1m9Zzs?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+    image: "london-property.jpg",
+    title: "Lovely flat in London",
+    price: 100,
+    location: {
+      firstLine: "16 Main Street",
+      city: "London",
+      code: 12345,
+      country: "Great Britain",
+    },
+    contact: "jane.smith@example.com",
+    isAvailable: true,
+  },
+  {
+    alt: "https://unsplash.com/photos/multicolored-concrete-houses-h95mT1m9Zzs?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+    image: "../colombia-property.jpg",
+    title: "Mountain Retreat in Switzerland",
+    price: 120,
+    location: {
+      firstLine: "789 Alpine Way",
+      city: "Zurich",
+      code: 54321,
+      country: "Switzerland",
+    },
+    contact: "michael.jones@example.com",
     isAvailable: true,
   },
 ];
+const propertyCards = properties.map((property) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `<h3>${property.title}</h3>`;
+  const image = document.createElement("img");
+  image.setAttribute("src", property.image);
+  card.appendChild(image);
+  return card;
+});
 
+propertyContainer?.append(...propertyCards);
 // Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
